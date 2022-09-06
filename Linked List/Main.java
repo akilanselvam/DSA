beacbc24fdd9e2d048e57170d1ce48dcb6650ed3
Akilan Selvam <54879150+akilanselvam@users.noreply.github.com>
2022-09-06 14:56:55 +0530

    Add files via upload

---
+++ Linked List/Main.java
@@ -0,0 +1,39 @@
+class Main {
+  public static void main(String[] args) {
+
+    //------Singly Linked List ---------------
+    // SinglyLinkedList sLL = new SinglyLinkedList();
+    // sLL.createSinglyLinkedList(5);
+    // sLL.insertLinkedList(6,0);
+    // sLL.insertLinkedList(7,3);
+    // sLL.insertLinkedList(4,2);
+    // sLL.insertLinkedList(23,1); 
+    // sLL.traverseLinkedList();
+    // sLL.searchNode(7);
+    // sLL.deletionOfNode(0);
+    //  sLL.traverseLinkedList();
+    // sLL.deleteEntireSSL();
+    // sLL.traverseLinkedList();
+    //------------------------------------------
+
+    //------Circular Singly Linked List ---------------
+    // CircularSinglyLinkedList csll = new CircularSinglyLinkedList();
+    // csll.createCSLL(5);
+    // csll.insertCSLL(25,1);
+    // csll.insertCSLL(15,1);
+    // csll.insertCSLL(30,13);
+    // System.out.println(csll.head.value);
+    // System.out.println(csll.head.next.value);
+    // System.out.println(csll.head.next.next.value);
+    // csll.traverseCSLL();
+    // System.out.println(" ");
+    // csll.searchNode(25);
+    // csll.deleteNode(2);
+    // csll.traverseCSLL();
+    // System.out.println(" ");
+    // csll.deleteEntireCSSL();
+    // csll.traverseCSLL();
+    //------------------------------------------
+    
+  }
+}
